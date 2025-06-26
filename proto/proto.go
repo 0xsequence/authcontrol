f691c8031a1620726388f8c5458faa4956836d6e
Vojtech Vitek (golang.cz) <vv@horizon.io>
2025-06-26 15:47:25 +0200

    Telemetry/Logs: Track requests by sessionType and projectID (#41)

--- proto/proto.go
+++ proto/proto.go
@@ -2,13 +2,17 @@
 //go:generate go run github.com/webrpc/webrpc/cmd/webrpc-gen -schema=authcontrol.ridl -target=typescript -client -out=./authcontrol.gen.ts
 package proto
 
-const SessionType_Max SessionType = SessionType_InternalService + 1
+const SessionType_Max SessionType = SessionType_S2S + 1
 
 // AndUp returns a list of all session types from the current one up to the maximum.
 func (s SessionType) OrHigher() []SessionType {
-	list := make([]SessionType, 0, SessionType_InternalService-s+1)
+	list := make([]SessionType, 0, SessionType_S2S-s+1)
 	for i := s; i < SessionType_Max; i++ {
 		list = append(list, i)
 	}
 	return list
 }
+
+// Deprecated: Use SessionType_S2S instead.
+// Note: Remove this after some time.
+const SessionType_InternalService = SessionType_S2S
