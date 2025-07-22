49ef47a350678fd2261e4d19a16868d95fefa172
Vojtech Vitek <vv@horizon.io>
2025-07-22 19:20:18 +0200

    Add jwt_token to service config, remove access_key

--- config.go
+++ config.go
@@ -1,7 +1,7 @@
 package authcontrol
 
 type ServiceConfig struct {
-	URL       string `toml:"url"`
-	JWTSecret string `toml:"jwt_secret"`
-	AccessKey string `toml:"access_key"`
+	URL       string `toml:"url"`        // Base URL of the service.
+	JWTSecret string `toml:"jwt_secret"` // Secret used to create JWT token for S2S authentication.
+	JWTToken  string `toml:"jwt_token"`  // Static JWT token used for authentication.
 }
